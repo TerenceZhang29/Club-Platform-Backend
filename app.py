2a70b7acae947ff6bfdd6f5fcf99cf353a5c920b
Janice Wei <42884384+Janicewei@users.noreply.github.com>
2020-06-24 17:09:24 -0400

    simple google login

--- app.py
+++ app.py
@@ -1,79 +1,153 @@
 import json
-from flask import Flask, request
-from db import db, Club
-import dao
+import os
+import sqlite3
 
-# define db filename
-db_filename = "data.db"
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from db import init_db_command
+from user import User
+
+# Configuration
+GOOGLE_CLIENT_ID = "282686073195-qnigrud47ph57lkmfqtmd0u3jftkrq4t.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "8xIPgZ-ru2f0Vr8zqObvXtc4"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# Flask app setup
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# setup config
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_filename}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
 
-# initialize app
-db.init_app(app)
-with app.app_context():
-    db.create_all()
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
-# generalized response formats
-def success_response(data, code=200):
-    return json.dumps({"success": True, "data": data}), code
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
-def failure_response(message, code=404):
-    return json.dumps({"success": False, "error": message}), code
-
-# --Club routes------
-
-# get all clubs
 @app.route("/")
-@app.route("/clubs/")
-def get_clubs():
-  return success_response(dao.get_clubs())
+def index():
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<div><p>Google Profile Picture:</p>"
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name, current_user.email, current_user.profile_pic
+            )
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
 
-# create a club
-@app.route("/clubs/", methods = ["POST"])
-def create_club():
-  body = json.loads(request.data)
-  name = body.get("name", "None")
-  link = body.get("link", "None")
-  industry = body.get("industry", "None")
-  email = body.get("email", "None")
-  phone = body.get("phone", "None")
-  about = body.get("about", "None")
-  location = body.get("location", "None")
-  registered_users = body.get("registered_users", 0)
-  club = dao.create_club(
-    name = name, 
-    link = link, 
-    industry = industry, 
-    email = email, 
-    phone = phone, 
-    about = about, 
-    location = location, 
-    registered_users = registered_users
-  )
-  return success_response(club, 201)
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
-# get a club by id
-@app.route("/club/<int:club_id>/")
-def get_club_by_id(club_id):
-  club = dao.get_club_by_id(club_id)
-  if club is None:
-    return failure_response("Club with id: " + str(club_id) + " not found !")
-  return success_response(club)
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-# update a club by id
-@app.route("/club/<int:club_id>/", methods = ["POST"])
-def update_club_by_id(club_id):
-  body = json.loads(request.data)
-  club = dao.update_club_by_id(club_id, body)
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
 
-  if club is None:
-    return failure_response("Club with id: " + str(club_id) + " not found !")
-  return success_response(club)
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
 
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
 
-if __name__ == '__main__':
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in your db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
--- db.py
+++ db.py
@@ -1,49 +1,38 @@
-from flask_sqlalchemy import SQLAlchemy
+# http://flask.pocoo.org/docs/1.0/tutorial/database/
+import sqlite3
 
-db = SQLAlchemy()
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 
-# Class for clubs
-# Parameters:
-# id: club id
-# name: club name
-# link: website link
-# industry: club industry
-# email: club email
-# phone: club phone
-# about: club about info
-# location: club location
-# registered_users: number of students who registered this club
-class Club(db.Model):
-  __tablename__ = "clubs"
-  id = db.Column(db.Integer, primary_key = True)
-  name = db.Column(db.String, nullable = False)
-  link = db.Column(db.String, nullable = False)
-  industry = db.Column(db.String, nullable = False)
-  email = db.Column(db.String, nullable = False)
-  phone = db.Column(db.String, nullable = False)
-  about = db.Column(db.Text, nullable = False)
-  location = db.Column(db.String, nullable = False)
-  registered_users = db.Column(db.Integer, nullable = False)
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            "sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
 
-  def __init__(self, **kwargs):
-    self.name = kwargs.get("name", "None")
-    self.link = kwargs.get("link", "None")
-    self.industry = kwargs.get("industry", "None")
-    self.email = kwargs.get("email", "None")
-    self.phone = kwargs.get("phone", "None")
-    self.about = kwargs.get("about", "None")
-    self.location = kwargs.get("location", "None")
-    self.registered_users = kwargs.get("registered_users", 0)
+    return g.db
 
-  def serialize(self):
-    return {
-      "id": self.id,
-      "name": self.name,
-      "link": self.link,
-      "industry": self.industry,
-      "email": self.email,
-      "phone": self.phone,
-      "about": self.about,
-      "location": self.location,
-      "registered_users": self.registered_users
-    }
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
